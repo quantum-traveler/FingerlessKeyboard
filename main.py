bcdfcde7d25d6e3d702486dce0db7001a354a311
crimsoncaveman <sarma017@umn.edu>
2024-07-05 12:46:28 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#create lexicon
+from english_words import get_english_words_set
+web2lowerset = get_english_words_set(['web2'], lower=True)
+
+print((web2lowerset))
+
+#associate each region with a finger-typing motion
+
+#L5 (left pinkie) - qaz
+#L4 - wsx
+#L3 - edc
+#L2 - rtfgv (b)
+#L1 (left thumb) - space
+#R1 (right thumb) - space
+#R2 - yhnmju
+#R3 - ik,
+#R4 - ol
+#R5 (right pinkie) - p
+
+#Object detection
+
+
+
+
